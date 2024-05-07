744dc5430a29bce0a269df7274d18d220db741da
Facu Areosa <facundo.areosa@gmail.com>
2024-05-07 14:00:17 +0200

    estilo cards

--- src/components/ShowResults.jsx
+++ src/components/ShowResults.jsx
@@ -4,21 +4,24 @@ import { useFetch } from './useFetch'
 function ShowResults() {
 
     const {data} = useFetch("https://api.jikan.moe/v4/top/anime?filter=favorite&type=tv")
-    const [topAnime, setTopAnime] = useState(null)
-  
+    const [topAnime, setTopAnime] = useState(null);
 
   useEffect(()=>{
     if (data && data.data) {
         setTopAnime(data.data);
     }
-  },[])
+  },[data])
 
   return (
-    <div>
-        {topAnime?.map((results)=>(
-            <div>
-                <h2>{results.titles[0].title}</h2>
-                <img src={results.images.webp.image_url} alt="" />
+    <div style={{display:'grid', gridTemplateColumns:'repeat 4(1fr)'}}>
+        {topAnime?.map((results) => (
+            <div className='card' key={results.mal_id}>
+                <h2 className='card__title'>{results.title}</h2>
+                <img className='card__img' src={results.images.webp.image_url} alt={"imagen de portada de " + results.title} />
+                {results.airing === true
+                    ? <p className='card__status'>al aire</p>
+                    : <p className='card__status'>Finalizado</p>
+                }
             </div>
         ))}
     </div>
